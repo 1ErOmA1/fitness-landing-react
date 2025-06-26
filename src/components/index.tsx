d752832c6b9e7a00e20887151cc2c9802704371b
1ErOmA1 <100420970+1ErOmA1@users.noreply.github.com>
2025-06-26 23:52:13 +0300

    create navbar

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,13 @@
 type Props = {}
 
 export const NavBar = (props: Props) => {
-	return <div>Наигация сайта</div>
+	return (
+		<nav className='relative h-[88px]'>
+			<div className='fixed top-0 z-30 w-full'>
+				<div className='mx-auto w-5/6 flex-between'>
+					<div className='flex-between gap-20 w-full'></div>
+				</div>
+			</div>
+		</nav>
+	)
 }
