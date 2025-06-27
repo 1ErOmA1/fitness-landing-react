a97f485e2b210a467f206a7f40871d1e7fa268ac
1ErOmA1 <100420970+1ErOmA1@users.noreply.github.com>
2025-06-27 23:41:59 +0300

    new images

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
+import Logo from '@/assets/Logo.png'
+
 type Props = {}
 
 export const NavBar = (props: Props) => {
@@ -5,7 +7,18 @@ export const NavBar = (props: Props) => {
 		<nav className='relative h-[88px]'>
 			<div className='fixed top-0 z-30 w-full'>
 				<div className='mx-auto w-5/6 flex-between'>
-					<div className='flex-between gap-20 w-full'></div>
+					<div className='flex-between gap-20 w-full'>
+						<img src={Logo} alt='Logo' />
+						<div className='flex-between w-full'>
+							<div className='flex-between gap-8 text-sm'>menu</div>
+							<div className='flex-between gap-8'>
+								<button className='rounded-md bg-primary-500 px-10 py-2 cursor-pointer hover:bg-secondary-500 hover:text-white animate'>
+									Sign in
+								</button>
+								<a href='' className=''></a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
